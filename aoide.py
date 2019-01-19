eaf2652348acb334d96281aa39864be138d11ba1
wtee <wtee@users.noreply.github.com>
2019-01-19 13:52:57 -0600

    Add Chord, Rest, Note-subclasses, and play_list

--- aoide.py
+++ aoide.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import simpleaudio as sa
 
 class Note:
     def __init__(self, pitch, duration):
@@ -7,8 +7,38 @@ class Note:
         self.duration = duration
 
 
+class SixteenthNote(Note):
+    def __init__(self, pitch):
+        self.pitch = pitch
+        self.duration = 0.0625
+
+
+class EighthNote(Note):
+    def __init__(self, pitch):
+        self.pitch = pitch
+        self.duration = 0.125
+
+
+class QuarterNote(Note):
+    def __init__(self, pitch):
+        self.pitch = pitch
+        self.duration = 0.25
+
+
+class HalfNote(Note):
+    def __init__(self, pitch):
+        self.pitch = pitch
+        self.duration = 0.5
+
+
+class WholeNote(Note):
+    def __init__(self, pitch):
+        self.pitch = pitch
+        self.duration = 1
+
+
 class Wave:
-    def __init__(self, form, sample_rate, note):
+    def __init__(self, sample_rate, tempo, note):
         """Accepts:
             sine
             triangle
@@ -17,18 +47,24 @@ class Wave:
             square
         """
         self.sample_rate = sample_rate
-        if form == "sine":
-            self.output = (
-                np.sin(
-                    2
-                    * np.pi
-                    * np.arange(self.sample_rate * note.duration)
-                    * note.pitch
-                    / self.sample_rate
-                )
-            ).astype(np.float32)
-        else:
-            print("Only the sine waveform is supported currently.")
+        # Beats per minute
+        self.tempo = tempo
+        self.duration = 100 / 60 * note.duration
+        self.pitch = note.pitch
+
+    def sine(self):
+        output = (
+            np.sin(
+                2
+                * np.pi
+                * np.arange(self.sample_rate * self.duration)
+                * self.pitch
+                / self.sample_rate
+            )
+        ).astype(np.float32)
+
+        return output
+
 
 class Rest:
     def __init__(self, duration):
@@ -37,7 +73,7 @@ class Rest:
 
 
 class Chord:
-    def __init__(self, notes, duration):
+    def __init__(self, duration, *notes):
         self.notes = sorted(notes)
         # Duration allows for simply playing the whole chord
         # for a span of time. Chords can also be used to 
@@ -45,3 +81,13 @@ class Chord:
         # as a convenient collection of notes with or without
         # regard to the duration propetry.
         self.duration = duration
+        self.notes = list(notes)
+
+
+def play_list(sample_rate, waves):
+    output = np.hstack(waves)
+    output *= 32767 / np.max(np.abs(output))
+    output = output.astype(np.int16)
+    play_obj = sa.play_buffer(output, 1, 2, sample_rate)
+    play_obj.wait_done()
+
--- example.py
+++ example.py
@@ -1,10 +1,20 @@
-#! /usr/bin/env python
-"""Plays an A4 for 1 second."""
+import numpy as np
+import simpleaudio as sa
 
-import sounddevice as sd
-from aoide import Note, Wave
+from aoide import EighthNote, QuarterNote, Wave, play_list
 
-a4 = Note(440, 1)
-wave = Wave("sine", 48000, a4)
+d5 = 587.3295
+a4 = 440.0000
+g4 = 391.9954
+e4 = 329.6276
+d4 = 293.6648
 
-sd.play(wave.output, wave.sample_rate, blocking=True)
+notes = [EighthNote(d4), EighthNote(e4), QuarterNote(g4), EighthNote(d4), EighthNote(e4), EighthNote(d4), QuarterNote(g4), QuarterNote(a4), EighthNote(a4), EighthNote(g4), QuarterNote(e4), EighthNote(d4), EighthNote(d4), EighthNote(g4), EighthNote(g4)]
+tempo = 60
+sample_rate = 48000
+
+wave_forms = [wave.sine() for wave in [Wave(sample_rate, tempo, note) for note in notes]]
+
+play_list(sample_rate, wave_forms)
+
+
